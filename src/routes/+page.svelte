f261f811f4ad63ae1bee1feefa6ef5e1b75d33af
Levi Jacobs <kontakt@levi-jacobs.de>
2026-01-19 07:34:39 +0100

    minimal reproduction example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a
+	href="/test"
+	onclick={(e) => {
+		e.preventDefault();
+		goto("/test#target");
+	}}>goto() Link</a>
+
+<a href="/test#target">Normal Link</a>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import "./page.css";
+</script>
+
+<header>Header</header>
+<div id="target">Target Element</div>
