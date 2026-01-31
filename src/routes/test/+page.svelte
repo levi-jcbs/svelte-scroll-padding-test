9309df1f86bf51021f52639b115941e770d8af3e
Levi Jacobs <kontakt@levi-jacobs.de>
2026-01-31 02:01:26 +0100

    Current Workaround

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+	import { afterNavigate } from "$app/navigation";
 	import "./page.css";
+
+	afterNavigate(async (navigation) => {
+		const hash = navigation.to?.url.hash;
+
+		if (navigation.type !== "enter" && hash) {
+			const anchor = document.getElementById(hash.slice(1));
+			if (anchor) {
+				setTimeout(() => {
+					anchor.scrollIntoView({
+						behavior: "instant",
+						inline: "start",
+						block: "start",
+					});
+				}, 0);
+			}
+		}
+	});
 </script>
 
 <header>Header</header>
